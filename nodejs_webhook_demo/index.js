c19039bdf11e2ac02086d807f16c2bde0e4c0dbf
hanalee-skytap <43791815+hanalee-skytap@users.noreply.github.com>
2019-01-25 14:05:53 -0800

    added command line option to send data to splunk (#5)

--- nodejs_webhook_demo/index.js
+++ nodejs_webhook_demo/index.js
@@ -2,11 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+var options = require('./util/options');
+var splunk = require('./util/splunk');
 var worker = require('./worker');
 
 app.use(bodyParser.json());
 
 app.post('/', function(req, res) {
+  if (options.args['splunk'] === true){
+    splunk.add(req.body.payload);
+  }
   worker.enqueue('processVM', 'process_running_vms', req.body.payload);
   res.status(200).send('OK');
 });
